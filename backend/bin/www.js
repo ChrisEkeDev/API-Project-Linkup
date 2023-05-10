cda1ed254cc06ba16bf0adac6a63472958263646
ChrisEkeDev <iam@chriseke.com>
2023-05-10 13:01:32 -0500

    Initialized set up of the server and connection to database

---
+++ backend/bin/www.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+// Import environemnt variables
+require('dotenv').config();
+const { port } = require('../config');
+const app = require('../app');
+const db = require('../db/models');
+
+// Check the database connection before starting the app
+db.sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Databse connection success! Sequelize is ready to use...');
+        app.listen(port, () => console.log(`Listening on port ${port}...`)) // Listen for connections
+    })
+    .catch(error => {
+        console.log('Database connection failure.');
+        console.error(error)
+    });
