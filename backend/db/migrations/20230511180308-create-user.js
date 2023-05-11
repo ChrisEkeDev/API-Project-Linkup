bcde1c6041e55cbf894dbab20ea0fc9f10e69731
ChrisEkeDev <iam@chriseke.com>
2023-05-11 15:06:10 -0500

    Add Users table migration and seeds

---
+++ backend/db/migrations/20230511180308-create-user.js
@@ -0,0 +1,67 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+      },
+      lastName: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+      },
+      username: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+        unique: true
+      },
+      email: {
+        type: Sequelize.STRING(256),
+        allowNull: false,
+        unique: true
+      },
+      hashedPassword: {
+        type: Sequelize.STRING.BINARY,
+        allowNull: false
+      },
+      profileImage: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      city: {
+        type: Sequelize.STRING(30),
+        defaultValue: null,
+      },
+      state: {
+        type: Sequelize.STRING(2),
+        defaultValue: null,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ backend/db/seeders/20230511193140-demo-user.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   options.tableName = 'Users';
+   await queryInterface.bulkInsert(options, [
+    {
+      firstName: 'Phil',
+      lastName: 'Cartwright',
+      username: 'cartyp',
+      email: 'pcartwirght@email.com',
+      hashedPassword: bcrypt.hashSync('password1'),
+    },
+    {
+      firstName: 'Beatrice',
+      lastName: 'Hobbs',
+      username: 'thequeenb',
+      email: 'bhobbs@email.com',
+      hashedPassword: bcrypt.hashSync('password2'),
+    },
+    {
+      firstName: 'Craig',
+      lastName: 'Mackey',
+      username: 'craigmack',
+      email: 'cmackey@email.com',
+      hashedPassword: bcrypt.hashSync('password3'),
+    },
+    {
+      firstName: 'Marshall',
+      lastName: 'Mitchell',
+      username: 'mandm',
+      email: 'mmitchell@email.com',
+      hashedPassword: bcrypt.hashSync('password4'),
+    },
+    {
+      firstName: 'Megan',
+      lastName: 'Ballion',
+      username: 'megtheballion',
+      email: 'mballion@email.com',
+      hashedPassword: bcrypt.hashSync('password5'),
+    },
+   ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete(options, {
+      username: { [Op.in] : ['cartyp', 'thequeenb', 'craigmack', 'mandm', 'megtheballion'] }
+    }, {})
+  }
+};
