838f37574d4909f12607617b55c13e379943002d
ChrisEkeDev <iam@chriseke.com>
2023-05-12 12:51:17 -0500

    Establised route connectionsboilerplate

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const userRouter = require('./users.js');
+const groupRouter = require('./groups.js');
+const venueRouter = require('./venues.js');
+const eventRouter = require('./events.js')
 const { restoreUser } = require('../../utils/auth.js');
 
 // Connect restoreUser middleware to the API router
@@ -8,8 +11,14 @@ const { restoreUser } = require('../../utils/auth.js');
   // If current user session is not valid, set req.user to null
 router.use(restoreUser)
 
-router.use('/session', sessionRouter)
+router.use('/session', sessionRouter);
 
-router.use('/users', userRouter)
+router.use('/users', userRouter);
+
+router.use('/groups', groupRouter);
+
+router.use('/venues', venueRouter);
+
+router.use('/events', eventRouter)
 
 module.exports = router;
