b41f51512f9fa552531f07aa45b3dea469279bc2
ChrisEkeDev <iam@chriseke.com>
2023-05-12 09:19:48 -0500

    Small changes to include firstName and lastName properties on authentication backend

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -31,6 +31,8 @@ router.post('/', validateSignUp, async (req, res) => {
 
     const safeUser = {
         id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
         email: user.email,
         username: user.username
     }
