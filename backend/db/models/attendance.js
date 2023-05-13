9ae0f8a53b8af8c616ab5925ada72e39eaa12b2d
ChrisEkeDev <iam@chriseke.com>
2023-05-13 12:17:24 -0500

    Created Attendances Table, Model, and Seeder files

---
+++ backend/db/models/attendance.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+  Model, Validator
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Attendance extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Attendance.init({
+    eventId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [['Waitlist', 'Attending']]
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Attendance',
+    defaultScope: {
+      attributes: {
+        exclude: ['createdAt', 'updatedAt']
+      }
+    }
+  });
+  return Attendance;
+};
