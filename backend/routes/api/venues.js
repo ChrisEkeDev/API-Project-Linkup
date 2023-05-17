03ab7027e9faa40685221c2c30d76943dd67be4d
ChrisEkeDev <iam@chriseke.com>
2023-05-17 10:17:35 -0500

    Finished Edit Venue endpoint

--- backend/routes/api/venues.js
+++ backend/routes/api/venues.js
@@ -1,10 +1,71 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const { Venue, User, Group, Membership } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
+const { states } = require('../../utils/states');
+
+
 
 // Edit a Venue specified by its id
-router.put('/:venueID', async (req, res) => {
-    const { venueID } = req.params;
-    res.json({route: `Update a venue with ID of ${venueID}`})
+const validateEditVenue = [
+    check('address').optional().exists({checkFalsy: true}).withMessage('Street address is required'),
+    check('city').exists().withMessage('City is required').optional(),
+    check('state').optional().exists({checkFalsy: true}).isIn(states).withMessage('State is required'),
+    check('lat').optional().exists({checkFalsy: true}).isFloat({ min: -90, max: 90 }).withMessage('Latitude is not valid'),
+    check('lng').optional().exists({checkFalsy: true}).isFloat({ min: -180, max: 180 }).withMessage('Longitude is not valid'),
+    handleValidationErrors
+]
+router.put('/:venueId', requireAuth, validateEditVenue, async (req, res) => {
+    const { venueId } = req.params;
+    const userId = req.user.id;
+    const venue = await Venue.findByPk(venueId);
+    const { address, city, state, lat, lng } = req.body;
+
+    // Checks if the group exists
+    if (!venue) {
+        res.status(404).json({
+            message: "Venue couldn't be found"
+        })
+    }
+
+    const groupId = venue.groupId;
+    const group = await Group.findByPk(groupId);
+
+    // Checks if user is a member of the group
+    let user = await group.getUsers({
+        where: {
+            id: userId,
+        }
+    });
+    if (user.length === 0) {
+        return res.status(403).json({
+            message: "Forbidden"
+        })
+    }
+
+    // Checks if user is the Organizer or the Co-host of the group
+    if ( user[0].dataValues.Membership.dataValues.status.toLowerCase() === "organizer"
+        || user[0].dataValues.Membership.dataValues.status.toLowerCase() === "co-host" ) {
+
+        //Updates the Venue
+        await venue.set({
+            address: address ? address : venue.address,
+            city: city ? city : venue.city,
+            state: state ? state : venue.state,
+            lat: lat ? lat : venue.lat,
+            lng: lng ? lng : venue.lng
+        })
+        await venue.save();
+        return res.status(200).json(venue)
+
+    } else {
+        return res.status(403).json({
+            message: "Forbidden"
+        })
+    }
+
 })
 
 module.exports = router;
