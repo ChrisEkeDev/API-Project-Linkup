eb5b69e5ddbe265db2a006ad22681478f0e7cef2
ChrisEkeDev <iam@chriseke.com>
2023-05-17 16:16:28 -0500

    Finished Edit Event endpoint

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -130,7 +130,7 @@ router.post('/:eventId/images', requireAuth, validateImage,  async (req, res) =>
     // console.log(user)
     // return res.status(200).json(user)
 
-    // Checks if user is the Organizer or the Co-host of the group
+    // Authorization
     let status = await user[0].dataValues.Attendance.dataValues.status.toLowerCase();
     if (status === 'attending' || status === 'host' || status === 'co-host') {
         // Creates the image
@@ -148,9 +148,84 @@ router.post('/:eventId/images', requireAuth, validateImage,  async (req, res) =>
 
 
 // Edit an Event specified by its id
-router.put('/:eventID', async (req, res) => {
-    const { eventID } = req.params;
-    res.json({route: `Update event with ID of ${eventID}`})
+const validateEditEvent = [
+    check('venueId').optional().custom(async(id) => {
+        const venue = await Venue.findByPk(id);
+        if (!venue) throw new Error('Venue does not exist')
+    }),
+    check('name').optional().exists({checkFalsy: true}).isLength({min: 5}).withMessage('Name must be at leat 5 characters'),
+    check('type').optional().exists({checkFalsy: true}).isIn(['In person', 'Online']).withMessage('Type must be Online or In person'),
+    check('capacity').optional().exists({checkFalsy: true}).isInt().withMessage('Capacity must be an integer'),
+    check('price').optional().custom(async (price) => {
+        let priceRegex = /^\d+(?:\.\d+)?(?:,\d+(?:\.\d{2})?)*$/;
+        if (!priceRegex.test(price)) throw new Error('Price is invalid')
+    }),
+    check('description').optional().exists({checkFalsy: true}).withMessage('Description is required'),
+    check('startDate').optional().custom(async (date) => {
+        let convDate = new Date(date);
+        let currDate = new Date();
+        if (convDate < currDate) throw new Error('Start date must be in the future')
+    }),
+    check('endDate').optional().custom(async (date, {req}) => {
+        let convDate = new Date(date);
+        let startDate = new Date(req.body.startDate)
+        // console.log(req.)
+        if (convDate < startDate) throw new Error('End date is less than start date')
+    }),
+    handleValidationErrors
+]
+
+router.put('/:eventId', requireAuth, validateEditEvent, async (req, res) => {
+    const { eventId } = req.params;
+    const userId = req.user.id;
+    const { venueId, name, type, capacity, price, description, startDate, endDate } = req.body;
+    let event = await Event.findByPk(eventId);
+
+    // Checks if event exists
+    if (!event) {
+        return res.status(404).json({
+            message: "Event couldn't be found"
+        })
+    }
+
+    const groupId = event.groupId;
+    const group = await Group.findByPk(groupId)
+
+    // Checks if user is attending the event
+    let user = await event.getUsers({
+        where: {
+            id: userId
+        }
+    });
+    if (user.length === 0) {
+        return res.status(403).json({
+            message: "Forbidden"
+        })
+    }
+
+    // Authorization
+    let status = await user[0].dataValues.Attendance.dataValues.status.toLowerCase();
+    console.log(status)
+    if (status === 'host' || status === 'co-host' || userId === group.organizerId) {
+
+        // Update event
+        await event.set({
+            venueId: venueId ? venueId : event.venueId,
+            name: name ? name : event.name,
+            type: type ? type : event.type,
+            capacity: capacity ? capacity : event.capacity,
+            price: price ? price : event.price,
+            description: description ? description : event.description,
+            startDate: startDate ? startDate : event.startDate,
+            endDate: endDate ? endDate : event.endDate
+        })
+        await event.save();
+        return res.status(200).json(event)
+    } else {
+        return res.status(403).json({
+            message: "Forbidden"
+        })
+    }
 })
 
 
