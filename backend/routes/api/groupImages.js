a79f5fa6a3ece2866fd4548ec2f9cf3611cb917c
ChrisEkeDev <iam@chriseke.com>
2023-05-18 08:53:50 -0500

    Finished Delete group image endpoint

--- backend/routes/api/groupImages.js
+++ backend/routes/api/groupImages.js
@@ -1,10 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const  { Group, GroupImage } = require('../../db/models')
+const { requireAuth } = require('../../utils/auth')
 
 // Delete an Image for a Group
-router.delete('/:imageID', async (req, res) => {
-    const { imageID } = req.params;
-    res.json({route: `Deletes an image for group with ID of ${imageID}`})
+router.delete('/:imageId', requireAuth, async (req, res) => {
+    const { imageId } = req.params;
+    const userId = req.user.id;
+    const image = await GroupImage.findByPk(imageId);
+
+
+    if (!image) {
+        return res.status(404).json({
+            message: "Group Image couldn't be found"
+        })
+    }
+
+    const groupId = image.groupId;
+    const group = await Group.findByPk(groupId);
+
+    const membership = group.getMemberships({
+        where: {
+            userId: userId
+        }
+    })
+
+    let status;
+    if (membership[0]) {
+        status = membership[0].status
+    }
+
+    if (userId === group.organizerId || status === 'co-host') {
+        await image.destroy();
+        return res.status(200).json({
+            message: "Successfully deleted"
+        })
+    } else {
+        return res.status(403).json({
+            message: 'Forbidden'
+        })
+    }
+
 })
 
 module.exports = router;
