3f698bfb5e2621d316283ad2818c5a7987292c5f
ChrisEkeDev <iam@chriseke.com>
2024-01-25 07:47:36 -0600

    Added href for client side Google Auth

--- client/src/views/auth/hooks/useOAuth2.js
+++ client/src/views/auth/hooks/useOAuth2.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+const isProduction = process.env.NODE_ENV === 'production';
 function useOAuth2() {
 
     const googleAuth = (e) => {
         e.preventDefault()
-        window.location.href = 'http://localhost:8000/api/auth/google';
+        window.location.href = isProduction
+            ? 'https://linkup-api-jw4b.onrender.com/api/auth/google'
+            : 'http://localhost:8000/api/auth/google';
     }
 
     const appleAuth = (e) => {
