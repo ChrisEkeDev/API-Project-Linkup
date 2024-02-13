1c065f80145161e23679b2d6170aee9d602f456f
ChrisEkeDev <iam@chriseke.com>
2024-02-13 12:13:37 -0600

    Fixed delete team modal

--- client/src/views/teams/components/DeleteTeamModal.js
+++ client/src/views/teams/components/DeleteTeamModal.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import { PiXBold, PiTrashBold} from 'react-icons/pi';
 import Button from '../../../components/shared/button'
-import ProfileImage from '../../../components/shared/profileImage';
-import { format, parseISO } from 'date-fns';
 
 
-function DeleteTeamModal({close, deleteTeam, member}) {
-    const { name } = member.Player;
-    const formatDate = format(parseISO(member.createdAt), 'MM/yyyy')
-
+function DeleteTeamModal({close, deleteTeam}) {
     return (
         <div className='modal_container'>
             <h2 className='md modal_title'>
@@ -18,22 +13,6 @@ function DeleteTeamModal({close, deleteTeam, member}) {
                 This cannot be undone. All messages and data shared in the team will
                 be deleted. Forever. Do you still want to do it?
             </p>
-            <div className='float_left'>
-                <ProfileImage player={member.Player}/>
-                <div>
-                    <div className='flex'>
-                        <p className='bold md'>{name}</p>
-                        {/* {
-                            showStatus &&
-                            <div className={`member_status member-${member.status}`}>
-                            {isMemberHost ? <PiStarFill className='xs'/> : isMemberCoHost ? <PiStarBold className="xs"/> : null }
-                            {showStatus ? <p className='xxs bold'>{member.status}</p> : null}
-                            </div>
-                        } */}
-                    </div>
-                    <p className='xs'>Joined {formatDate}</p>
-                </div>
-            </div>
             <div className='modal_actions'>
                 <Button
                     label="Keep Team"
